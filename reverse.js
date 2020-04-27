b8b202ec7a80044b14c00831ac449c830e573cd0
John Kim <kimjon0509@gmail.com>
2020-04-27 23:12:32 +0000

    reverse string

--- reverse.js
+++ reverse.js
@@ -1,10 +1,10 @@
 const args = process.argv;
 let newArr = args.slice(2, args.length);
 
-const reverse = function(array){
-  let reverseStr = ''
-  for(let i = 0; i < array.length; i++) {
-    for(let j = array[i].length - 1; j >= 0; j--) {
+const reverse = function(array) {
+  let reverseStr = '';
+  for (let i = 0; i < array.length; i++) {
+    for (let j = array[i].length - 1; j >= 0; j--) {
       reverseStr += array[i][j];
     }console.log(reverseStr);
     reverseStr = '';
