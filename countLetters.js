809347b3354275cc762b7b625312078313440ec0
John Kim <kimjon0509@gmail.com>
2020-04-29 18:04:41 +0000

    fixed error when string argument has spaces

--- countLetters.js
+++ countLetters.js
@@ -19,6 +19,8 @@ const assertEqual = function(actual, expected) {
 
 const countLetters = function(string) {
   const result = {};
+  string = string.split(/\s/).join('');
+  console.log(string)
   for (letter of string) {
     if (!(letter in result)) {
       result[letter] = 1;
@@ -28,9 +30,10 @@ const countLetters = function(string) {
   } return result;
 };
 
-let word = countLetters('hello');
+let word = countLetters('hello my name is');
 assertEqual(word['h'] , 1);
-assertEqual(word['e'] , 1);
+assertEqual(word['e'] , 2);
 assertEqual(word['l'] , 2);
 assertEqual(word['o'] , 1);
-assertEqual(word['m'] , undefined);
+assertEqual(word['m'] , 2);
+assertEqual(word['k'] , undefined);
