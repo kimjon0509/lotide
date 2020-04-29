0a8c7fa804f8faec1272c1a270b358b0c60d940c
John Kim <kimjon0509@gmail.com>
2020-04-29 17:57:25 +0000

    initialised countLetter function

---
+++ countLetters.js
@@ -0,0 +1,36 @@
+const assertEqual = function(actual, expected) {
+  actual = typeof actual === 'string' ? `"${actual}"` : actual;
+  expected = typeof expected === 'string' ? `"${expected}"` : expected;
+  
+  // ternary operator
+  // condition ? true case : false case
+
+  if (actual === expected) {
+    // console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+
+  } else {
+    // console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !==  ${expected}`);
+    
+  }
+};
+
+const countLetters = function(string) {
+  const result = {};
+  for (letter of string) {
+    if (!(letter in result)) {
+      result[letter] = 1;
+    } else {
+      result[letter] += 1;
+    }
+  } return result;
+};
+
+let word = countLetters('hello');
+assertEqual(word['h'] , 1);
+assertEqual(word['e'] , 1);
+assertEqual(word['l'] , 2);
+assertEqual(word['o'] , 1);
+assertEqual(word['m'] , undefined);
