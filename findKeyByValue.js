c04d582b13871c975d9989dbd06e123588aa1bf8
John Kim <kimjon0509@gmail.com>
2020-05-04 21:05:15 +0000

    added module.exports

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,28 +1,10 @@
-const assertEqual = function(actual, expected) {
-  actual = typeof actual === 'string' ? `"${actual}"` : actual;
-  expected = typeof expected === 'string' ? `"${expected}"` : expected;
-
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !==  ${expected}`);
-  }
-};
-
 const findKeyByValue = function(object, value) {
   for (key in object) {
-    if(object[key] === value) {
+    if (object[key] === value) {
       return key
     }
-  }
-  return undefined
+  } 
+  return undefined;
 };
 
-const bestTVShowsByGenre = { 
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
-
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+module.exports =findKeyByValue;
