3c5de74ba2309e8ffbdade35dc62bc20bb837a63
John Kim <kimjon0509@gmail.com>
2020-05-04 16:45:28 +0000

    added module.exports to eqArrays function

--- eqArrays.js
+++ eqArrays.js
@@ -1,30 +1,9 @@
-const assertEqual = function(actual, expected) {
-  actual = typeof actual === 'string' ? `"${actual}"` : actual;
-  expected = typeof expected === 'string' ? `"${expected}"` : expected;
-  
-  // ternary operator
-  // condition ? true case : false case
-
-  if (actual === expected) {
-    // console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
-
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-
-  } else {
-    // console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !==  ${expected}`);
-    
-  }
-};
-
 const eqArrays = function(firstArray, secondArray) {
-  for (let i = 0; i < firstArray.length; i++){
-    if (firstArray[i] !== secondArray[i]) {
-      return false;
-    }
-  }return true;
+  if (firstArray.length !== secondArray.length) return false;
+  for (let item in firstArray){
+    if (firstArray[item] !== secondArray[item]) return false;
+  }
+  return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true)
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false)
+module.exports = eqArrays;
